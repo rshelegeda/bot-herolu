d927ec2d191374ce75479fca1fc2a43f790603c3
Roman Shell <rshelegeda@ukr.net>
2023-08-05 18:22:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+// const ViberBot = require('viber-bot').Bot;
+// const BotEvents = require('viber-bot').Events;
+// const TextMessage = require('viber-bot').Message.Text;
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const botToken = '516ff9caa967e753-dd2295bc24ac206b-d624986d90a59abf';
+const port = 3000;
+
+
+// const bot = new ViberBot({
+//     authToken: botToken,
+//     name: 'MyViberBot',
+//     avatar: 'https://bipbap.ru/wp-content/uploads/2017/04/000f_7290754.jpg',
+// });
+
+// // Обработчик события нового сообщения
+// bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+//     console.log('Message');
+//     // Отвечаем на каждое сообщение пользователя текстом "Привет!"
+//     response.send(new TextMessage('Привет!'));
+// });
+
+// const webhookUrl = "https://viberbot-tmorvac3ma-uc.a.run.app";
+
+
+
+// app.post('/webhook', bot.middleware());
+
+app.post('/sendData', (req, res) => {
+    return res.send('Hello from POST');
+});
+
+app.get('/getData', (req, res) => {
+    return res.send('Hello from GET');
+});
+
+app.listen(port, () => {
+    console.log(`Бот слушает порт ${port}`);
+
+    // // Устанавливаем вебхуки после того, как сервер запущен и прослушивает порт
+    // try {
+    //     bot.setWebhook(webhookUrl).catch((error) => {
+    //         console.error('Ошибка установки вебхука:', error);
+    //     })
+       
+    // } catch (error) {
+    //     console.log(error)
+    // }
+});
+
