a7060dd0cc11284cde2e43fb8418af723a526aa4
Roman Shell <rshelegeda@ukr.net>
2023-08-05 19:07:58 +0300

    9

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 
-// const ViberBot = require('viber-bot').Bot;
-// const BotEvents = require('viber-bot').Events;
-// const TextMessage = require('viber-bot').Message.Text;
+const ViberBot = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
+const TextMessage = require('viber-bot').Message.Text;
 const express = require('express');
 require('dotenv').config();
 
@@ -13,24 +13,24 @@ const botToken = '516ff9caa967e753-dd2295bc24ac206b-d624986d90a59abf';
 
 
 
-// const bot = new ViberBot({
-//     authToken: botToken,
-//     name: 'MyViberBot',
-//     avatar: 'https://bipbap.ru/wp-content/uploads/2017/04/000f_7290754.jpg',
-// });
+const bot = new ViberBot({
+    authToken: botToken,
+    name: 'MyViberBot',
+    avatar: 'https://bipbap.ru/wp-content/uploads/2017/04/000f_7290754.jpg',
+});
 
-// // Обработчик события нового сообщения
-// bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-//     console.log('Message');
-//     // Отвечаем на каждое сообщение пользователя текстом "Привет!"
-//     response.send(new TextMessage('Привет!'));
-// });
+// Обработчик события нового сообщения
+bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+    console.log('Message');
+    // Отвечаем на каждое сообщение пользователя текстом "Привет!"
+    response.send(new TextMessage('Привет!'));
+});
 
-// const webhookUrl = "https://viberbot-tmorvac3ma-uc.a.run.app";
+const webhookUrl = "https://cryptic-shore-64677-904cb630fee4.herokuapp.com";
 
 
 
-// app.post('/webhook', bot.middleware());
+app.post('/webhook', bot.middleware());
 
 app.get('/', (req, res) => {
     res.send('Using global content security policy!');
